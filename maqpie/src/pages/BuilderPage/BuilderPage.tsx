29979f27d874c603d3f58c04cf3ad100495585de
Jaehun You <jaehun0121@snu.ac.kr>
2025-07-09 01:09:42 +0900

    Fetch items under root
    See also: #2

--- maqpie/src/pages/BuilderPage/BuilderPage.tsx
+++ maqpie/src/pages/BuilderPage/BuilderPage.tsx
@@ -5,6 +5,8 @@ import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import Stack from "@mui/material/Stack";
 
+import Explorer from "./components/Explorer/explorer";
+
 export default function BuilderPage() {
   const [openExplorer, setOpenExplorer] = useState(false);
 
@@ -26,7 +28,7 @@ export default function BuilderPage() {
           Explorer
         </DialogTitle>
         <DialogContent>
-          Temp
+          <Explorer />
         </DialogContent>
       </Dialog>
     </Stack>
--- maqpie/src/pages/BuilderPage/components/Explorer/explorer.tsx
+++ maqpie/src/pages/BuilderPage/components/Explorer/explorer.tsx
@@ -1,19 +1,67 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
 
 interface TreeNode {
   id: string;
   name: string;
-  children?: TreeNode[];
+  isExperiment: boolean;
+  parent?: TreeNode;
+  children: TreeNode[];
+}
+
+function getTreePath(node: TreeNode) {
+  const path: string[] = [];
+  while (node.parent) {
+    path.unshift(node.name);
+    node = node.parent;
+  }
+  return path.join('/');
 }
 
 export default function Explorer() {
   const [tree, setTree] = useState<TreeNode[]>([]);
+  const rootNode: TreeNode = {
+    id: '.',
+    name: '',
+    isExperiment: false,
+    children: [],
+  };
+
+  useEffect(() => {
+    fetchRoot();
+  }, []);
+
+  const makeSubTree = (data: string[], parent: TreeNode) => {
+    const filteredData = data.filter((name: string) => (
+      !name.startsWith('_') && (name.endsWith('/') || name.endsWith('.py'))
+    ));
+
+    return filteredData.map((name: string) => ({
+      id: `${getTreePath(parent)}/${name}`,
+      name: name,
+      isExperiment: name.endsWith('.py'),
+      children: [],
+      parent: parent,
+    }));
+  };
+
+  const fetchRoot = async () => {
+    const url = new URL('/ls/', window.location.origin);
+    url.searchParams.set('directory', '.');
+
+    const res = await fetch(url.toString(), {
+      method: 'GET',
+    });
+    const data = await res.json();
+    
+    setTree(makeSubTree(data, rootNode));
+  };
 
   const renderTree = (nodes: TreeNode[]) =>
     nodes.map((node) => (
       <TreeItem
+        key={node.id}
         itemId={node.id}
         label={node.name}
       >
