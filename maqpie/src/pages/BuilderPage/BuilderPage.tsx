934cc89b8ac5f69818b8cf4f3bba25ba5e914f8b
Jaehun You <jaehun0121@snu.ac.kr>
2025-07-09 00:30:05 +0900

    Add dialog for explorer
    See also: #2

--- maqpie/src/pages/BuilderPage/BuilderPage.tsx
+++ maqpie/src/pages/BuilderPage/BuilderPage.tsx
@@ -1,3 +1,34 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import Stack from "@mui/material/Stack";
+
 export default function BuilderPage() {
-  return <div>Builder</div>;
+  const [openExplorer, setOpenExplorer] = useState(false);
+
+  return (
+    <Stack>
+      <Stack direction="row" justifyContent="flex-end" spacing={2}>
+        <Button
+          variant="contained"
+          onClick={() => setOpenExplorer(true)}
+        >
+          Explorer
+        </Button>
+      </Stack>
+      <Dialog
+        onClose={() => setOpenExplorer(false)}
+        open={openExplorer}
+      >
+        <DialogTitle>
+          Explorer
+        </DialogTitle>
+        <DialogContent>
+          Temp
+        </DialogContent>
+      </Dialog>
+    </Stack>
+  );
 }
