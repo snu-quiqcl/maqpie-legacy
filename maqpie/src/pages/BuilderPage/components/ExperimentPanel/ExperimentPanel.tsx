f1a8883bfb9c7b3a172cce7bff6a22e2b294b86d
Jaehun You <jaehun0121@snu.ac.kr>
2025-07-16 19:58:08 +0900

    Modify experiment panel style
    See also: #9

--- maqpie/src/pages/BuilderPage/components/ExperimentPanel/ExperimentPanel.tsx
+++ maqpie/src/pages/BuilderPage/components/ExperimentPanel/ExperimentPanel.tsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Stack from '@mui/material/Stack';
+import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import Tab from '@mui/material/Tab';
 import TextField from '@mui/material/TextField';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
@@ -34,70 +36,74 @@ export default function ExperimentPanel({ experiment }: ExperimentPanelProps) {
 
   return (
     <Card id={experiment.id} variant='outlined'>
-      <Tooltip title={experiment.path}>
-        <Typography variant='h6'>{experiment.cls}</Typography>
-      </Tooltip>
-      <TextField
-        label='Tag'
-        variant='standard'
-        value={experiment.tag}
-        onChange={(event) => handleTagChange(event, experiment.id)}
-      />
-      <Typography variant='subtitle1'>{experiment.name}</Typography>
-      <Box>
-        <TabContext value={experimentTab}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList
-              centered
-              onChange={(_, value) => setExperimentTab(value)}
-            >
-              <Tab label='Arguments' value='args' />
-              <Tab label='Scheduling Options' value='schedOpts' />
-            </TabList>
-          </Box>
-          <TabPanel value='args'>
-            {experiment.args.map((arg) => {
-              if (arg.kind === 'BooleanArg') {
-                return (
-                  <BooleanArgInput
-                    key={arg.id}
-                    experimentId={experiment.id}
-                    arg={arg}
-                  />
-                );
-              } else if (arg.kind === 'EnumerationArg') {
-                return (
-                  <EnumerationArgInput
-                    key={arg.id}
-                    experimentId={experiment.id}
-                    arg={arg}
-                  />
-                );
-              } else if (arg.kind === 'NumberArg') {
-                return (
-                  <NumberArgInput
-                    key={arg.id}
-                    experimentId={experiment.id}
-                    arg={arg}
-                  />
-                );
-              } else if (arg.kind === 'StringArg') {
-                return (
-                  <StringArgInput
-                    key={arg.id}
-                    experimentId={experiment.id}
-                    arg={arg}
-                  />
-                );
-              }
-              throw new Error(`Unknown argument kind: ${arg.kind}`);
-            })}
-          </TabPanel>
-          <TabPanel value='schedOpts'>
-            Scheduling Options
-          </TabPanel>
-        </TabContext>
-      </Box>
+      <CardContent>
+        <Tooltip title={experiment.path}>
+          <Typography variant='h6'>{experiment.cls}</Typography>
+        </Tooltip>
+        <TextField
+          label='Tag'
+          variant='standard'
+          value={experiment.tag}
+          onChange={(event) => handleTagChange(event, experiment.id)}
+        />
+        <Typography variant='subtitle1'>{experiment.name}</Typography>
+        <Box>
+          <TabContext value={experimentTab}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <TabList
+                centered
+                onChange={(_, value) => setExperimentTab(value)}
+              >
+                <Tab label='Arguments' value='args' />
+                <Tab label='Scheduling Options' value='schedOpts' />
+              </TabList>
+            </Box>
+            <TabPanel value='args'>
+              <Stack spacing={2}>
+                {experiment.args.map((arg) => {
+                  if (arg.kind === 'BooleanArg') {
+                    return (
+                      <BooleanArgInput
+                        key={arg.id}
+                        experimentId={experiment.id}
+                        arg={arg}
+                      />
+                    );
+                  } else if (arg.kind === 'EnumerationArg') {
+                    return (
+                      <EnumerationArgInput
+                        key={arg.id}
+                        experimentId={experiment.id}
+                        arg={arg}
+                      />
+                    );
+                  } else if (arg.kind === 'NumberArg') {
+                    return (
+                      <NumberArgInput
+                        key={arg.id}
+                        experimentId={experiment.id}
+                        arg={arg}
+                      />
+                    );
+                  } else if (arg.kind === 'StringArg') {
+                    return (
+                      <StringArgInput
+                        key={arg.id}
+                        experimentId={experiment.id}
+                        arg={arg}
+                      />
+                    );
+                  }
+                  throw new Error(`Unknown argument kind: ${arg.kind}`);
+                })}
+              </Stack>
+            </TabPanel>
+            <TabPanel value='schedOpts'>
+              Scheduling Options
+            </TabPanel>
+          </TabContext>
+        </Box>
+      </CardContent>
     </Card>
   );
 }
