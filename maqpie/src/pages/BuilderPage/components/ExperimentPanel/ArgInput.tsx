8bcde568e80a733accdef93b5d4c2dec202c5e65
Jaehun You <jaehun0121@snu.ac.kr>
2025-07-16 19:01:29 +0900

    Implement enumeration arg input
    See also: #9

--- maqpie/src/pages/BuilderPage/components/ExperimentPanel/ArgInput.tsx
+++ maqpie/src/pages/BuilderPage/components/ExperimentPanel/ArgInput.tsx
@@ -1,11 +1,20 @@
 import { useDispatch } from 'react-redux';
 import Box from '@mui/material/Box';
 import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { type SelectChangeEvent } from '@mui/material/Select';
 import Tooltip from '@mui/material/Tooltip';
 
 import type { AppDispatch } from '../../../../store';
-import { experimentActions, type Arg, type BooleanArg } from '../../../../store/slices/experiment/experiment';
+import {
+  experimentActions,
+  type Arg,
+  type BooleanArg,
+  type EnumerationArg
+} from '../../../../store/slices/experiment/experiment';
 
 type ArgInputProps = {
   experimentId: string;
@@ -36,6 +45,40 @@ export function BooleanArgInput({ experimentId, arg: arg_ }: ArgInputProps) {
           }
           label={arg.name}
         />
+      </Tooltip>
+    </Box>
+  );
+}
+
+export function EnumerationArgInput({ experimentId, arg: arg_ }: ArgInputProps) {
+  const arg = arg_ as EnumerationArg;
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    dispatch(experimentActions.updateArg({
+      experimentId,
+      argId: arg.id,
+      arg: { ...arg, value: event.target.value },
+    }));
+  };
+
+  return (
+    <Box>
+      <Tooltip title={[arg.tooltip, `(Default: ${arg.default})`].filter(Boolean).join('\n')}>
+        <FormControl fullWidth>
+          <InputLabel id={`${arg.id}-select-label`}>{arg.name}</InputLabel>
+          <Select
+            labelId={`${arg.id}-select-label`}
+            value={arg.value}
+            onChange={handleChange}
+          >
+            {arg.choices.map((choice) => (
+              <MenuItem key={choice} value={choice}>
+                {choice}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
       </Tooltip>
     </Box>
   );
