416b0e992e39e9e5d302cc8490b739ffd4e2757c
Jaehun You <jaehun0121@snu.ac.kr>
2025-07-21 01:53:14 +0900

    Construct schedule state
    See also: #17

--- maqpie/src/store/slices/schedule/schedule.ts
+++ maqpie/src/store/slices/schedule/schedule.ts
@@ -1,10 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import type { DateTime } from 'luxon';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 import type { RootState } from '../..';
 
+export type RunStatus = (
+  'pending' | 'preparing' | 'prepare_done' | 'running' | 'run_done' | 'analyzing' | 'deleting' |
+  'paused'
+);
+
+export interface Run {
+  rid: number;
+  cls: string | null;
+  status: RunStatus;
+  pipeline: string;
+  priority: number;
+  due_date: DateTime | null;
+  args: object;
+}
+
+export interface ScheduleState {
+  runs: Run[];
+}
+
+const initialState: ScheduleState = {
+  runs: [],
+};
+
 export const scheduleSlice = createSlice({
   name: 'schedule',
-  initialState: {},
+  initialState,
   reducers: {},
 });
 
